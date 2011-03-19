ac336a310177e2ec6d873722c828303b4c3b5ba8
nyuhuhuu <nyuhuhuu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-19 13:11:02 +0000

    Fix plugin file name and function prefixes to conform plugin name.
    git-svn-id: http://plugins.svn.wordpress.org/intranet-restriction-for-posts-and-pages/trunk@362141 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- intranet-restriction-for-posts-and-pages.php
+++ intranet-restriction-for-posts-and-pages.php
@@ -11,50 +11,50 @@ Author URI: http://webdevil.hu/
  * @author slink
  */
 
-register_activation_hook( __FILE__,'wd_intranet_install' );
-register_deactivation_hook( __FILE__, 'wd_intranet_remove' );
+register_activation_hook( __FILE__,'intranet_restriction_install' );
+register_deactivation_hook( __FILE__, 'intranet_restriction_remove' );
 
-add_filter( 'the_posts', 'wd_intranet_filter_posts', 1 );
-add_filter( 'get_pages', 'wd_intranet_filter_posts', 1 );
+add_filter( 'the_posts', 'intranet_restriction_filter_posts', 1 );
+add_filter( 'get_pages', 'intranet_restriction_filter_posts', 1 );
 
-add_action( 'edit_post', 'wd_intranet_update' );
-add_action( 'save_post', 'wd_intranet_update' );
-add_action( 'publish_post', 'wd_intranet_update' );
+add_action( 'edit_post', 'intranet_restriction_update' );
+add_action( 'save_post', 'intranet_restriction_update' );
+add_action( 'publish_post', 'intranet_restriction_update' );
 
 add_action( 'admin_menu', 'wd_add_custom_box' );
-add_action( 'admin_menu', 'wd_intranet_admin_settings_menu' );
+add_action( 'admin_menu', 'intranet_restriction_admin_settings_menu' );
 
 
-function wd_intranet_install() {
-    add_option( 'wd_intranet_data', "192.168.0.0/255.255.0.0\n10.0.0.0/255.0.0.0", '', 'yes' );
+function intranet_restriction_install() {
+    add_option( 'intranet_restriction_data', "192.168.0.0/255.255.0.0\n10.0.0.0/255.0.0.0", '', 'yes' );
 }
 
 
-function wd_intranet_remove() {
-    delete_option( 'wd_intranet_data' );
+function intranet_restriction_remove() {
+    delete_option( 'intranet_restriction_data' );
 }
 
 
-function wd_intranet_admin_settings_menu() {
+function intranet_restriction_admin_settings_menu() {
     add_options_page(
         __('Intranet Restriction'),
         __('Intranet Restriction'),
         'administrator',
         'wd-intranet',
-        'wd_intranet_admin_settings_page'
+        'intranet_restriction_admin_settings_page'
     );
 }
 
-function wd_intranet_admin_settings_page() {
+function intranet_restriction_admin_settings_page() {
     ?>
     <div class="wrap">
         <h2><?php _e('Intranet Restriction Settings'); ?></h2>
         <p><?php _e('Specify domain names and IP ranges. Put each of them in separate line.'); ?> </p>
         <form method="post" action="options.php">
         <?php wp_nonce_field( 'update-options' ); ?>
-        <textarea cols="50" rows="10" name="wd_intranet_data"><?php echo get_option( 'wd_intranet_data' ); ?></textarea>
+        <textarea cols="50" rows="10" name="intranet_restriction_data"><?php echo get_option( 'intranet_restriction_data' ); ?></textarea>
         <input type="hidden" name="action" value="update" />
-        <input type="hidden" name="page_options" value="wd_intranet_data" />
+        <input type="hidden" name="page_options" value="intranet_restriction_data" />
         <br/><input type="submit" value="<?php _e('Save Changes') ?>" />
         </form>
    </div>
@@ -64,8 +64,8 @@ function wd_intranet_admin_settings_page() {
 /**
  * Filters posts
  */
-function wd_intranet_filter_posts( $posts ) {
-    if ( is_admin() || wd_intranet_is_intranet() ) {
+function intranet_restriction_filter_posts( $posts ) {
+    if ( is_admin() || intranet_restriction_is_intranet() ) {
         return $posts;
     }
     $filtered = array();
@@ -84,8 +84,8 @@ function wd_intranet_filter_posts( $posts ) {
  */
 function wd_add_custom_box() {
     if ( function_exists( 'add_meta_box' ) ) {
-        add_meta_box( 'wd_intranet', __('Intranet'), 'wd_inner_custom_box', 'page', 'normal', 'high' );
-        add_meta_box( 'wd_intranet', __('Intranet'), 'wd_inner_custom_box', 'post', 'normal', 'high' );
+        add_meta_box( 'intranet_restriction', __('Intranet'), 'wd_inner_custom_box', 'page', 'normal', 'high' );
+        add_meta_box( 'intranet_restriction', __('Intranet'), 'wd_inner_custom_box', 'post', 'normal', 'high' );
     }
 }
 
@@ -94,7 +94,7 @@ function wd_add_custom_box() {
  * Prints the inner fields for the custom post/page section
  */
 function wd_inner_custom_box() {
-  echo '<input type="hidden" name="wd_intranet_noncename" id="wd_intranet_noncename" value="' . wp_create_nonce( plugin_basename(__FILE__) ) . '" />';
+  echo '<input type="hidden" name="intranet_restriction_noncename" id="intranet_restriction_noncename" value="' . wp_create_nonce( plugin_basename(__FILE__) ) . '" />';
 
   global $post;
   $restrict_intranet = get_post_meta( $post->ID, 'wd_restrict_intranet', true );
@@ -114,10 +114,10 @@ function wd_inner_custom_box() {
 /**
  * Saves data
  */
-function wd_intranet_update( $id ) {
+function intranet_restriction_update( $id ) {
     // verify this came from the our screen and with proper authorization,
     // because save_post can be triggered at other times
-    if ( ! wp_verify_nonce( $_POST['wd_intranet_noncename'], plugin_basename(__FILE__) ) ) {
+    if ( ! wp_verify_nonce( $_POST['intranet_restriction_noncename'], plugin_basename(__FILE__) ) ) {
         return $post_id;
     }
 
@@ -138,7 +138,7 @@ function wd_intranet_update( $id ) {
 }
 
 
-function wd_intranet_match_network( $nets, $ip, $first=false ) {
+function intranet_restriction_match_network( $nets, $ip, $first=false ) {
    $return = false;
    if ( ! is_array ($nets) ) {
        $nets = array ($nets);
@@ -168,8 +168,8 @@ function wd_intranet_match_network( $nets, $ip, $first=false ) {
 /**
  * Returns true if client is from intranet
  */
-function wd_intranet_is_intranet() {
-    $settings = wd_intranet_parse_data();
+function intranet_restriction_is_intranet() {
+    $settings = intranet_restriction_parse_data();
     // check reverse names
     if ( count( $settings['reverse'] ) > 0 ) {
         $client_reverse = gethostbyaddr( $_SERVER['REMOTE_ADDR'] );
@@ -181,7 +181,7 @@ function wd_intranet_is_intranet() {
     // check ip ranges
     if ( count( $settings['ipmask'] ) > 0 ) {
         foreach( $settings['ipmask'] as $mask ) {
-            if ( wd_intranet_match_network( $mask, $_SERVER['REMOTE_ADDR'] ) ) {
+            if ( intranet_restriction_match_network( $mask, $_SERVER['REMOTE_ADDR'] ) ) {
                 return TRUE;
             }
         }
@@ -193,8 +193,8 @@ function wd_intranet_is_intranet() {
 /**
  * Parses settings data and returns IP mask and reverse list
  */
-function wd_intranet_parse_data() {
-    $data = explode( "\n", get_option( 'wd_intranet_data' ) );
+function intranet_restriction_parse_data() {
+    $data = explode( "\n", get_option( 'intranet_restriction_data' ) );
     $ipmask_list = array();
     $reverse_list = array();
     foreach ( $data as $line ) {
